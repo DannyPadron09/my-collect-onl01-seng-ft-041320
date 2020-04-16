409be0059579a815077f52bab294acf0f81dbc81
Danny Padron <danny.padron09@gmail.com>
2020-04-16 15:51:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -5,9 +5,11 @@ def my_collect(array)
   yielded_values = []
   #While loop
   while i < array.length
-
+    #inside of the while loop shovel in yielded return values into array
+    yielded_values << yield(array[i])
+    #increment by one
+    i += 1
   end
-  #inside of the while loop shovel in yielded return values into array
-  #increment by one
+  yielded values 
   #outside of while loop return array with yielded values
 end
