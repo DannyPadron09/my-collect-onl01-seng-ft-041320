6fdb415af4576d481138b9b83a455b976edbb423
Danny Padron <danny.padron09@gmail.com>
2020-04-16 16:00:19 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,15 @@
-def my_collect(array)
-  #Set initial i value
-  i = 0
-  #Creat new array to store return values of yield
-  yielded_values = []
-  #While loop
-  while i < array.length
-    #inside of the while loop shovel in yielded return values into array
-    yielded_values << yield(array[i])
-    #increment by one
-    i += 1
-  end
-  yielded values
-  #outside of while loop return array with yielded values
-end
+ def my_collect(arr)
+   # Set initial i value
+   i = 0
+   # Create new array to store return values of yield
+   yielded_values = []
+   #  while loop
+   while i < arr.length
+     # inside of the while loop shovel in yielded return values into array
+     yielded_values << yield(arr[i])
+     # increment by one
+     i += 1
+   end
+   yielded_values
+   # outside of while loop return array with yielded values
+ end
